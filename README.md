9211963b92b8ae830347fe22b6a86efc69daf3f6
Vincent Linux <vincent.berthet42@gmail.com>
2019-10-24 09:02:15 +0200

    tutorial

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 *BERTHET Vincent*
 
 *QUERO Benoit*
+
+## Tutorial
+[tutorial.py](tutorial.py)
+### Debevec (ToneMap)
+![](output/ldr_debevec.jpg)
+### Robertson (ToneMap)
+![](output/ldr_robertson.jpg)
+### Mertens
+![](output/fusion_mertens.jpg)
--- tutorial.py
+++ tutorial.py
@@ -1,15 +1,34 @@
 import cv2 as cv
 import numpy as np
-# Loading exposure images into a list
+
+# 0.Require
+# pip install opencv-python==3.2.0.8
+
+# 1.Loading exposure images into a list
 img_fn = ["exposure_fusion/samples/peyrou_over.jpg", "exposure_fusion/samples/peyrou_under.jpg", "exposure_fusion/samples/peyrou_over.jpg"]
 img_list = [cv.imread(fn) for fn in img_fn]
-#exposure_times = np.array([15.0, 2.5, 0.25], dtype=np.float32)
+exposure_times = np.array([15.0, 2.5, 0.25], dtype=np.float32)
 
+# 2.Merge exposures to HDR image
+merge_debevec = cv.createMergeDebevec()
+hdr_debevec = merge_debevec.process(img_list, times=exposure_times.copy())
+merge_robertson = cv.createMergeRobertson()
+hdr_robertson = merge_robertson.process(img_list, times=exposure_times.copy())
 
-# Exposure fusion using Mertens
+# 3.Tonemap HDR image
+tonemap1 = cv.createTonemapDurand(gamma=2.2)
+res_debevec = tonemap1.process(hdr_debevec.copy())
+tonemap2 = cv.createTonemapDurand(gamma=1.3)
+res_robertson = tonemap2.process(hdr_robertson.copy())
+
+# 4.Exposure fusion using Mertens
 merge_mertens = cv.createMergeMertens()
 res_mertens = merge_mertens.process(img_list)
 
-# Convert datatype to 8-bit and save
+# 5.Convert datatype to 8-bit and save
+res_debevec_8bit = np.clip(res_debevec*255, 0, 255).astype('uint8')
+cv.imwrite("output/ldr_debevec.jpg", res_debevec_8bit)
+res_robertson_8bit = np.clip(res_robertson*255, 0, 255).astype('uint8')
+cv.imwrite("output/ldr_robertson.jpg", res_robertson_8bit)
 res_mertens_8bit = np.clip(res_mertens*255, 0, 255).astype('uint8')
 cv.imwrite("output/fusion_mertens.jpg", res_mertens_8bit)
