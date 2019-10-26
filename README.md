4111f9f20d1086126929258e087bd611f96cb847
Vincent Linux <vincent.berthet42@gmail.com>
2019-10-27 00:45:17 +0200

    tuto

--- README.md
+++ README.md
@@ -7,8 +7,12 @@
 ## Tutorial
 [tutorial.py](tutorial.py)
 ### Debevec (ToneMap)
+Luminance physique
 ![](output/ldr_debevec.jpg)
 ### Robertson (ToneMap)
+Luminance physique
 ![](output/ldr_robertson.jpg)
 ### Mertens
+Brigthness
 ![](output/fusion_mertens.jpg)
+
--- tutorial.py
+++ tutorial.py
@@ -1,13 +1,15 @@
 import cv2 as cv
 import numpy as np
+#import matplotlib.pyplot as plt
 
 # 0.Require
 # pip install opencv-python==3.2.0.8
+# pip install matplotlib.pyplot
 
 # 1.Loading exposure images into a list
-img_fn = ["exposure_fusion/samples/peyrou_over.jpg", "exposure_fusion/samples/peyrou_under.jpg", "exposure_fusion/samples/peyrou_over.jpg"]
+img_fn = ["images/ref/img3.jpg","images/ref/img2.jpg","images/ref/img1.jpg","images/ref/img0.jpg"]
 img_list = [cv.imread(fn) for fn in img_fn]
-exposure_times = np.array([15.0, 2.5, 0.25], dtype=np.float32)
+exposure_times = np.array([15.0, 2.5, 0.25, 0.0333], dtype=np.float32)
 
 # 2.Merge exposures to HDR image
 merge_debevec = cv.createMergeDebevec()
@@ -25,7 +27,15 @@ res_robertson = tonemap2.process(hdr_robertson.copy())
 merge_mertens = cv.createMergeMertens()
 res_mertens = merge_mertens.process(img_list)
 
-# 5.Convert datatype to 8-bit and save
+# 5.Estimate camera response function (CRF)
+cal_debevec = cv.createCalibrateDebevec()
+crf_debevec = cal_debevec.process(img_list, times=exposure_times)
+hdr_debevec = merge_debevec.process(img_list, times=exposure_times.copy(), response=crf_debevec.copy())
+cal_robertson = cv.createCalibrateRobertson()
+crf_robertson = cal_robertson.process(img_list, times=exposure_times)
+hdr_robertson = merge_robertson.process(img_list, times=exposure_times.copy(), response=crf_robertson.copy())
+
+# 6.Convert datatype to 8-bit and save
 res_debevec_8bit = np.clip(res_debevec*255, 0, 255).astype('uint8')
 cv.imwrite("output/ldr_debevec.jpg", res_debevec_8bit)
 res_robertson_8bit = np.clip(res_robertson*255, 0, 255).astype('uint8')
